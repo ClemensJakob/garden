da306e55689f6b2403fa40d1554cc34f162680a1
clemensenit <155433572+clemensenit@users.noreply.github.com>
2026-05-22 18:10:25 +0200

    style: remove padding

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,9 +6,9 @@ import GardenLayout from '../components/GardenLayout'
  */
 export default function MainPage() {
   return (
-    <main className="flex flex-col justify-center items-center gap-6 bg-gray-50 p-2 min-h-screen">
-      <h1 className="sr-only">Garden</h1>
+    // <main className="flex flex-col justify-center items-center gap-6 bg-gray-50 min-h-screen">
+    //   <h1 className="sr-only">Garden</h1>
       <GardenLayout />
-    </main>
+    // </main>
   )
 }
