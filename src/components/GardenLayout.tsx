911d708e939e59194fa0fd9bb8b70e082b6e87a6
clemensenit <155433572+clemensenit@users.noreply.github.com>
2026-05-23 08:31:18 +0200

    feat: adapt layout for readability

--- src/components/GardenLayout.tsx
+++ src/components/GardenLayout.tsx
@@ -19,35 +19,49 @@ function patch(n: number) {
 export default function GardenLayout() {
   return (
     <div
-      className="grid w-full h-[100dvh] max-w-md gap-2 grid-cols-12 grid-rows-[repeat(22,minmax(0,1fr))] bg-garden-bg p-2"
+      className="grid h-[100dvh] w-full max-w-md grid-cols-12 grid-rows-[repeat(22,minmax(0,1fr))] gap-2 bg-garden-bg p-2"
       role="region"
       aria-label="Garten-Layout"
     >
-      <PatchTile patch={patch(11)}     className="col-start-1 col-span-5 row-start-1 row-span-1" />
-      <PatchTile patch={patch(10)}  className="col-start-1 col-span-5 row-start-2 row-span-2" />
-      <PatchTile patch={patch(9)}   className="col-start-1 col-span-5 row-start-4 row-span-2" />
-      <PatchTile patch={patch(8)}   className="col-start-1 col-span-5 row-start-6 row-span-2" />
-      <PatchTile patch={patch(7)}   className="col-start-1 col-span-5 row-start-8 row-span-2" />
-      <PatchTile patch={patch(6)}   className="col-start-1 col-span-5 row-start-10 row-span-2" />
-      <PatchTile patch={patch(5)}   className="col-start-1 col-span-5 row-start-12 row-span-2" />
-      <PatchTile patch={patch(4)}   className="col-start-1 col-span-5 row-start-14 row-span-2" />
-      <PatchTile patch={patch(3)}   className="col-start-1 col-span-5 row-start-16 row-span-2" />
-      <PatchTile patch={patch(2)}   className="col-start-1 col-span-5 row-start-18 row-span-2" />
-      <PatchTile patch={patch(1)}   className="col-start-1 col-span-5 row-start-20 row-span-2" />
+      <PatchTile patch={patch(11)} className="col-span-5 col-start-1 row-span-1 row-start-1" />
+      <PatchTile patch={patch(10)} className="col-span-5 col-start-1 row-span-2 row-start-2" />
+      <PatchTile patch={patch(9)} className="col-span-5 col-start-1 row-span-2 row-start-4" />
+      <PatchTile patch={patch(8)} className="col-span-5 col-start-1 row-span-2 row-start-6" />
+      <PatchTile patch={patch(7)} className="col-span-5 col-start-1 row-span-2 row-start-8" />
+      <PatchTile patch={patch(6)} className="col-span-5 col-start-1 row-span-2 row-start-10" />
+      <PatchTile patch={patch(5)} className="col-span-5 col-start-1 row-span-2 row-start-12" />
+      <PatchTile patch={patch(4)} className="col-span-5 col-start-1 row-span-2 row-start-14" />
+      <PatchTile patch={patch(3)} className="col-span-5 col-start-1 row-span-2 row-start-16" />
+      <PatchTile patch={patch(2)} className="col-span-5 col-start-1 row-span-2 row-start-18" />
+      <PatchTile patch={patch(1)} className="col-span-5 col-start-1 row-span-2 row-start-20" />
 
       {/*Mitte*/}
-      <PatchTile patch={patch(12)} className="col-start-6 col-span-4 row-start-1" />
-      <PatchTile patch={patch(13)} className="col-start-7 col-span-4 row-start-5 row-span-11" />
+      <PatchTile patch={patch(12)} className="col-span-4 col-start-6 row-start-1" />
+      <PatchTile
+        id="tomatenbeet"
+        patch={patch(13)}
+        className="col-span-3 col-start-7 row-span-11 row-start-5"
+      />
 
       {/*Rechts*/}
-      <PatchTile patch={patch(14)} className="col-start-11 col-span-3 row-start-5 row-span-11" />
-      <PatchTile patch={patch(15)} className="col-start-10 col-span-4 row-start-1" />
-      <PatchTile patch={patch(16)} className="col-start-10 col-span-3 row-start-2 row-span-3" />
+      <PatchTile
+        id="gemuesehuegel"
+        patch={patch(14)}
+        className="col-span-3 col-start-10 row-span-11 row-start-5"
+      />
+      <PatchTile patch={patch(15)} className="col-span-4 col-start-10 row-start-1" />
+      <PatchTile patch={patch(16)} className="col-span-3 col-start-10 row-span-3 row-start-2" />
 
-
-      <PatchTile patch={patch(17)} className="col-start-7 col-span-6 row-start-20 row-span-2" />
-      <StorageUnitTile unit={STORAGE_UNITS.find((u) => u.id === 'schuppen')!} className="col-start-10 col-span-3 row-start-17 row-span-2" />
-      <StorageUnitTile unit={STORAGE_UNITS.find((u) => u.id === 'kiste')!} className="col-start-7 col-span-3 row-start-17 row-span-2" />
+      {/*Unten*/}
+      <PatchTile patch={patch(17)} className="col-span-6 col-start-7 row-span-2 row-start-20" />
+      <StorageUnitTile
+        unit={STORAGE_UNITS.find((u) => u.id === 'schuppen')!}
+        className="col-span-3 col-start-10 row-span-2 row-start-17"
+      />
+      <StorageUnitTile
+        unit={STORAGE_UNITS.find((u) => u.id === 'kiste')!}
+        className="col-span-3 col-start-7 row-span-2 row-start-17"
+      />
     </div>
   )
 }
